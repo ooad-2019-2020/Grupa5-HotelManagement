00fca8c41d19f8ef2a6a8511995d83f69d22df25
Belmin <bhuseinovi1@etf.unsa.ba>
2020-05-18 11:51:30 +0200

    test

--- E-hotel-implementacija/E-hotel-implementacija/Program.cs
+++ E-hotel-implementacija/E-hotel-implementacija/Program.cs
@@ -14,6 +14,8 @@ namespace E_hotel_implementacija
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

f190ccc514a6ed8303b6910a084f187e00df86b3
Vahidin <vhasic1@etf.unsa.ba>
2020-06-05 15:51:23 +0200

    Promijenjen prikaz labela za sobu

---
+++ E-hotel-implementacija/E-hotel-implementacija/Models/Rezervacija.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_hotel_implementacija.Models
+{
+    public class Rezervacija
+    {
+        [Key]
+        public int RezervacijaId { get; set; }
+
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Datum početka boravka")]
+        public DateTime DatumPocetka { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Datum kraja boravka")]
+        public DateTime DatumKraja { get; set; }
+
+        [Display(Name = "Popust u procentima")]
+        public int Popust { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Datum rezervacije")]
+        public DateTime DatumRezervacije { get; set; }
+
+
+        [Display(Name = "Redni broj parking mjesta")]
+        public int Parking { get; set; }
+
+
+        [Display(Name = "Validnost rezervacije")]
+        public bool Validnost { get; set; }
+    }
+}
--- E-hotel-implementacija/E-hotel-implementacija/Models/Soba.cs
+++ E-hotel-implementacija/E-hotel-implementacija/Models/Soba.cs
@@ -12,12 +12,16 @@ namespace E_hotel_implementacija.Models
         [Key]
         public int SobaId { get; set; }
 
+        [Display(Name = "Redni broj sobe")]
         public string Brojsobe { get; set; }
 
+        [Display(Name = "Cijena sobe po danu u KM")]
         public double CijenaSobe { get; set; }
 
+        [Display(Name = "Ocjena sobe")]
         public double OcjenaSobe { get; set; }
 
+        [Display(Name = "Opis sobe")]
         public string OpisSobe { get; set; }
 
         [Display(Name = "Slika sobe")]
